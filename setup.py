672fdfca320d54345334e19b08e4864919d4fece
Vansh Jain <vanshjain19878@gmail.com>
2026-05-09 17:38:30 +0530

    initial structure and push

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="scrapper",
+    version="0.0.1",
+    author="vansh",
+    author_email="vanshjain19878@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
