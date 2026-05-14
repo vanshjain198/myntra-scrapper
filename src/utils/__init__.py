9cea91bedc03bab5ee3c8a229178b89c79db5531
Vansh Jain <vanshjain19878@gmail.com>
2026-05-14 23:26:33 +0530

    Initial commit

---
+++ src/utils/__init__.py
@@ -0,0 +1,13 @@
+from src.cloud_io import MongoIO
+from src.constants import MONGO_DATABASE_NAME
+from src.exception import CustomException
+import os, sys
+
+
+def fetch_product_names_from_cloud():
+    try:
+        mongo = MongoIO()
+        collection_names = mongo.mongo_ins._mongo_operation__connect_database.list_collection_names()
+        return [name.replace('_', ' ') for name in collection_names]
+    except Exception as e:
+        raise CustomException(e, sys)
